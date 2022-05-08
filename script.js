622e50726beb8e79c5ef674706757bb9b0fd57f2
Isabelle <hatingusernames97@gmail.com>
2022-05-07 22:13:33 -0500

    Messed up

--- script.js
+++
@@ -1,7 +0,0 @@
-// create two variables called firstName, Lastname
-let firstName = "Isabelle"
-let lastName = "Link"
-// concatenate these two variables into a 3rd called fullName
-let fullName = firstName + lastName
-// console.log fullName
-console.log(fullName)
